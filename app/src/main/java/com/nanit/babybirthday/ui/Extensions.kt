0a4ea77aa6340bb511ebac3951ce9729059e488a
nikolay-vasilev-stanga <136490717+nikolay-vasilev-stanga@users.noreply.github.com>
2025-03-28 14:31:30 +0200

    Details Page

---
+++ app/src/main/java/com/nanit/babybirthday/ui/Extensions.kt
@@ -0,0 +1,16 @@
+package com.nanit.babybirthday.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun Dp.dpToPx() = with(LocalDensity.current) { this@dpToPx.toPx() }
+
+
+@Composable
+fun Int.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
+
+
+@Composable
+fun Float.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
